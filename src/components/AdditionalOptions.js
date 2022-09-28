e932bf532835e4d44fbd136614a4ada2628eeeab
shankart24 <itsshankart44@gmail.com>
2022-09-28 21:46:00 +0530

    Single handler function for additional options

--- src/components/AdditionalOptions.js
+++ src/components/AdditionalOptions.js
@@ -1,21 +1,25 @@
 export const AdditionalOptions = ({ state, setState }) => {
+    const handleOptions = (e, type, fieldName) => {
+        setState(state => ({
+            ...state,
+            additionalOptions: {
+                ...state.additionalOptions,
+                [type]: {
+                    ...state.additionalOptions[type],
+                    [fieldName]: e.target.value
+                }
+            }
+        }));
+    };
+
     if (state.strikeCriteria === "Strike Type") {
         return (
             <div>
                 <p>Strike Type</p>
                 <select
-                    onChange={e => {
-                        setState(state => ({
-                            ...state,
-                            additionalOptions: {
-                                ...state.additionalOptions,
-                                "Strike Type": {
-                                    ...state.additionalOptions["Strike Type"],
-                                    strikeType: e.target.value
-                                }
-                            }
-                        }));
-                    }}
+                    onChange={e =>
+                        handleOptions(e, "Strike Type", "strikeType")
+                    }
                     name={"Strike Type"}
                 >
                     {["ATM", "ITM9", "ITM10", "ITM11", "ITM12", "ITM13"]?.map(
@@ -40,20 +44,7 @@ export const AdditionalOptions = ({ state, setState }) => {
             <div style={{ display: "flex", gap: "10px", marginTop: "40px" }}>
                 [ <span>ATM Strike</span>{" "}
                 <select
-                    onChange={e => {
-                        setState(state => ({
-                            ...state,
-                            additionalOptions: {
-                                ...state.additionalOptions,
-                                "Straddle Width": {
-                                    ...state.additionalOptions[
-                                        "Straddle Width"
-                                    ],
-                                    type: e.target.value
-                                }
-                            }
-                        }));
-                    }}
+                    onChange={e => handleOptions(e, "Straddle Width", "type")}
                     name={"Straddle Width"}
                 >
                     {["+", "-"]?.map(optionName => {
@@ -72,20 +63,9 @@ export const AdditionalOptions = ({ state, setState }) => {
                 <input
                     type="number"
                     value={state.additionalOptions["Straddle Width"].multiplier}
-                    onChange={e => {
-                        setState(state => ({
-                            ...state,
-                            additionalOptions: {
-                                ...state.additionalOptions,
-                                "Straddle Width": {
-                                    ...state.additionalOptions[
-                                        "Straddle Width"
-                                    ],
-                                    multiplier: Number(e.target.value)
-                                }
-                            }
-                        }));
-                    }}
+                    onChange={e =>
+                        handleOptions(e, "Straddle Width", "multiplier")
+                    }
                     id="multiplier"
                     name="multiplier"
                 />{" "}
@@ -103,20 +83,9 @@ export const AdditionalOptions = ({ state, setState }) => {
                         value={
                             state.additionalOptions["Premium Range"].lowerRange
                         }
-                        onChange={e => {
-                            setState(state => ({
-                                ...state,
-                                additionalOptions: {
-                                    ...state.additionalOptions,
-                                    "Premium Range": {
-                                        ...state.additionalOptions[
-                                            "Premium Range"
-                                        ],
-                                        lowerRange: Number(e.target.value)
-                                    }
-                                }
-                            }));
-                        }}
+                        onChange={e =>
+                            handleOptions(e, "Premium Range", "lowerRange")
+                        }
                         id="lowerRange"
                         name="lowerRange"
                     />
@@ -128,20 +97,9 @@ export const AdditionalOptions = ({ state, setState }) => {
                         value={
                             state.additionalOptions["Premium Range"].upperRange
                         }
-                        onChange={e => {
-                            setState(state => ({
-                                ...state,
-                                additionalOptions: {
-                                    ...state.additionalOptions,
-                                    "Premium Range": {
-                                        ...state.additionalOptions[
-                                            "Premium Range"
-                                        ],
-                                        upperRange: Number(e.target.value)
-                                    }
-                                }
-                            }));
-                        }}
+                        onChange={e =>
+                            handleOptions(e, "Premium Range", "upperRange")
+                        }
                         id="upperRange"
                         name="upperRange"
                     />
@@ -156,20 +114,9 @@ export const AdditionalOptions = ({ state, setState }) => {
                 <input
                     type="number"
                     value={state.additionalOptions["Closest Premium"].premium}
-                    onChange={e => {
-                        setState(state => ({
-                            ...state,
-                            additionalOptions: {
-                                ...state.additionalOptions,
-                                "Closest Premium": {
-                                    ...state.additionalOptions[
-                                        "Closest Premium"
-                                    ],
-                                    premium: Number(e.target.value)
-                                }
-                            }
-                        }));
-                    }}
+                    onChange={e =>
+                        handleOptions(e, "Closest Premium", "premium")
+                    }
                     id="premium"
                     name="premium"
                 />
